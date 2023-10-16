828e9352aae25db4cad5c6c7789817940e995592
Sumi N <sumilibinl77@gmail.com>
2023-10-16 21:30:57 +0530

    final commit

--- surveyform/script.js
+++ surveyform/script.js
@@ -1,19 +1,47 @@
 
         function submitForm() {
-            const form = document.getElementById('surveyForm');
-            const resultContent = document.getElementById('resultContent');
+           const form = document.getElementById('surveyForm');
+           const resultContent = document.getElementById('resultContent');
 
            
             const firstName = document.getElementById('firstName').value;
+            if(!firstName){
+                alert('Please fill up First Name.');
+                return;
+            }
             const lastName = document.getElementById('lastName').value;
-            const dob = document.getElementById('dob').value;
+            if(!lastName){
+                alert('Please fill up Last Name.');
+                return;
+            }
+           const dob = document.getElementById('dob').value;
+           if(!dob){
+            alert('Please fill up Date of Birth.');
+            return;
+        }
             const country = document.getElementById('country').value;
+            if(!firstName){
+                alert('Please select country name.');
+                return;
+            }
             const genderElements = document.querySelectorAll('input[name="gender"]:checked');
             const profession = document.getElementById('profession').value;
+            if(!profession){
+                alert('Please fill up profession.');
+                return;
+            }
             const email = document.getElementById('email').value;
+            if(!email){
+                alert('Please fill up email.');
+                return;
+            }
             const mobile = document.getElementById('mobile').value;
+            if(!mobile){
+                alert('Please fill up mobile.');
+                return;
+            } 
 
-            
+             
             let gender = '';
             if (genderElements.length > 0) {
                 genderElements.forEach(element => {
@@ -24,7 +52,9 @@
                 alert('Please select at least one gender.');
                 return;
             }
-
+            
+            alert('Form submitted successfully!');
+            form.reset();               
             
             const result = `
 
@@ -39,7 +69,7 @@
             `;
 
             resultContent.innerHTML = result;
-
+            
            
             const popup = document.getElementById('resultPopup');
             popup.style.display = 'block';
